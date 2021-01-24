25149bb4368556f3f83bb5e39e70ddd9394a0481
Victor Alvarenga de Moraes <victor.moraes@outlook.com.br>
2021-01-24 00:58:07 -0300

    Adicionar arquivos de projeto.

---
+++ src/ToDoList.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ToDoList.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
